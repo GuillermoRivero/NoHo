5492efc880916e096c6a45896b1f346e6b5f80a6
alu0100693386 <alu0100693386@ull.edu.es>
2015-01-16 18:27:11 +0000

    Coordinacion Hormigas-Mapa
    Arreglados un par de problemas en el algoritmo genetico y incluido el
    funcionamiento en la clase mapa. Asimismo, se ha añadido el metodo para
    obtener las feromonas en el mapa.

---
+++ src/Interfaz.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JFrame;
+
+/**
+ * Clase encargada de mostrar por pantalla.
+ */
+/**
+ * @author Gillermo Rivero
+ * @author Boris Ballester
+ * @author Cristian Luis
+ */
+public class Interfaz extends JFrame{
+    public Interfaz(){
+        super ();
+    }
+    
+    
+}
